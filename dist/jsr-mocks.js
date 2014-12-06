985de5a264e585143cfdd25e7f51d25e70be9090
Dan Shahin <dan@codescience.com>
2014-12-06 11:27:33 -0800

    add configurable timeout per mock

--- dist/jsr-mocks.js
+++ dist/jsr-mocks.js
@@ -8,8 +8,8 @@
 			Manager:{
 				invokeAction: function(){
 					var callback = arguments[arguments.length - 1],
-						mockMethod = $mocks[arguments[0]],
-						result = mockMethod(arguments),
+						mock = $mocks[arguments[0]],
+						result = mock.method(arguments),
 						event = {status : true};
 					setTimeout(function(){
 						callback(result,event);
