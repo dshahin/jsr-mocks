e03da7c8a58a49de86ff11aa7819591d838867f1
Dan Shahin <dan@codescience.com>
2015-01-07 19:16:30 -0800

    add conditional injection and optional jsr parameters

--- dist/jsr-mocks.js
+++ dist/jsr-mocks.js
@@ -1,21 +1,27 @@
-(function(global, $, $config){
+(function(global, $, configSettings){
 
-	var $mocks = $config.mocks;
 	
-	global.Visualforce = {
-		//Visualforce.remoting.Manager.invokeAction
-		remoting :{
-			Manager:{
-				invokeAction: function(){
-					var callback = arguments[arguments.length - 1],
-						mock = $mocks[arguments[0]],
-						result = mock.method(arguments),
-						event = {status : true};
-					setTimeout(function(){
-						callback(result,event);
-					},mock.timeout);
+	if(!global.Visualforce){
+		var $mocks = configSettings.mocks;
+		global.Visualforce = {
+			//Visualforce.remoting.Manager.invokeAction
+			remoting :{
+				Manager:{
+					invokeAction: function(){
+						var lastArg = arguments[arguments.length - 1],
+						    callback = lastArg,
+							mock = $mocks[arguments[0]],
+							result = mock.method(arguments),
+							event = {status : true};
+						if(typeof(callback) === 'object'){
+							callback = arguments[arguments.length - 2];
+						}
+						setTimeout(function(){
+							callback(result,event);
+						},mock.timeout);
+					}
 				}
 			}
 		}
 	}
-})(window, jQuery, $config);
+})(window, jQuery, configSettings);
