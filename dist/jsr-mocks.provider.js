cd75056a420487594ba567747d4966023de54431
Dan Shahin <dan@codescience.com>
2015-04-16 16:11:29 -0700

    add support for local json-server

--- dist/jsr-mocks.provider.js
+++ dist/jsr-mocks.provider.js
@@ -4,6 +4,8 @@ angular.module('jsrMocks', [])
     .provider('jsrMocks', jsrMocks)
     .factory('jsr',jsr);
 
+
+
 function jsrMocks() {
 
     var $mocks;
@@ -14,36 +16,90 @@ function jsrMocks() {
             $mocks = mocks;
         },
 
-        $get: function() {
+        $get: function($log,$http) {
             if(!window.Visualforce){
+                var mocker,
+                    mockType = typeof($mocks);
+                if(mockType === 'object'){
+                    mocker = invokeStaticAction;
+                    
+                }else{
+                    
+                    mocker = invokeHTTPAction;
+                }
+                    
                 return {
                     remoting: {
                         Manager: {
-                            invokeAction: function() {
-                                var lastArg = arguments[arguments.length - 1],
-                                    callback = lastArg,
-                                    mock = $mocks[arguments[0]],
-                                    result = mock.method(arguments),
-                                    event = {
-                                        status: true
-                                    };
-                                if (typeof(callback) === 'object') {
-                                    callback = arguments[arguments.length - 2];
-                                }
-                                setTimeout(function() {
-                                    callback(result, event);
-                                }, mock.timeout);
-                            }
+                            invokeAction: mocker
                         }
                     }
                 };
+                
             }else{
                 return Visualforce;
             }
+
+            function invokeStaticAction(){
+                $log.log('$mocks is an object:', $mocks);
+                var lastArg = arguments[arguments.length - 1],
+                    callback = lastArg,
+                    mock = $mocks[arguments[0]],
+                    result = mock.method(arguments),
+                    event = {
+                        status: true
+                    };
+                if (typeof(callback) === 'object') {
+                    callback = arguments[arguments.length - 2];
+                }
+                setTimeout(function() {
+                    callback(result, event);
+                }, mock.timeout);
+            }
+
+            function invokeHTTPAction(){
+                $log.log('$mocks is not an object:', mockType, $mocks);
+                var lastArg = arguments[arguments.length - 1],
+                    callback = lastArg,
+                    mockName = arguments[0],
+                    regex = /(\w*)?}/,
+                    match = regex.exec(mockName),
+                    mockName = match[0].split('\}')[0],
+                    url = $mocks + mockName,
+                    event = {
+                        status: true
+                    };;
+
+                if (typeof(callback) === 'object') {
+                    callback = arguments[arguments.length - 2];
+                }
+
+                $log.debug('http mock url:',url);
+
+                $http.get(url).
+                    success(function(data,status,headers,config){
+                        var result = data;
+                        $log.debug(data);
+                        setTimeout(function() {
+                            callback(result, event);
+                        }, 100);
+                    }). 
+                    error(function(data, status, headers, config) {
+                        alert('error');
+                        $log.debug(data, status, headers, config);
+                    });
+                    
+            }   
+
         }
 
+
     }
+
+
 }
+
+
 
 function jsr(jsrMocks,$q,$rootScope){
     var Visualforce = jsrMocks;
